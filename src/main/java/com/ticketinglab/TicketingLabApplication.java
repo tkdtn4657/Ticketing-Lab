7310bb32542a30ee52ffe19187fbadfba08c7e5f
tkdtn4657 <tkdtn101226@gmail.com>
2026-01-14 14:22:03 +0900

    init

---
+++ src/main/java/com/ticketinglab/TicketingLabApplication.java
@@ -0,0 +1,13 @@
+package com.ticketinglab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ticketinglab/TicketingLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketinglab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
